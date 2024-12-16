bebe7ce3c51c982b2077dc03400f92f08d16cb5e
AshishP-armar <apparmar4466@gmail.com>
2024-12-16 22:41:11 +0530

    allowed host updates

--- quiz_app_project/settings.py
+++ quiz_app_project/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j0s+s1gph645examu0a(5g%*s7c3xkqki=@9aimn82d@n$fbz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [127.0.0.0]
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
