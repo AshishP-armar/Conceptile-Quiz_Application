7f993b27f3e9816532ac9f41df1e6a456249c3fa
AshishP-armar <apparmar4466@gmail.com>
2024-12-16 22:59:40 +0530

    secret key add from env varible

--- quiz_app_project/settings.py
+++ quiz_app_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j0s+s1gph645examu0a(5g%*s7c3xkqki=@9aimn82d@n$fbz7'
+load_dotenv()
+# SECRET_KEY = 'django-insecure-j0s+s1gph645examu0a(5g%*s7c3xkqki=@9aimn82d@n$fbz7'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
